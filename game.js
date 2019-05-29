f768d0392bb42cdd3d6ed2721735b50a51e58679
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-29 21:57:09 +0100

    add syntax for player choice

---
+++ game.js
@@ -0,0 +1,25 @@
+const gameSummary = {
+  numbers: 0,
+  wins: 0,
+  losses: 0,
+  draws: 0
+}
+
+const game = {
+  playerHand: '',
+  aiHand: ''
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+//player choice function 
+function handSelection() {
+  game.playerHand = this.dataset.option;
+  console.log(game.playerHand)
+  hands.forEach(hand => {
+    hand.style.boxShadow = '';
+  })
+  this.style.boxShadow = '0 0 0 4px yellow'
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
