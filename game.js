19da96594609c87504149ce5cad58b8cc4e307ad
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-30 20:46:59 +0100

    add computer choice function

--- game.js
+++ game.js
@@ -16,10 +16,22 @@ const hands = [...document.querySelectorAll('.select img')];
 function handSelection() {
   game.playerHand = this.dataset.option;
   console.log(game.playerHand)
-  hands.forEach(hand => {
-    hand.style.boxShadow = '';
-  })
+  hands.forEach(hand => hand.style.boxShadow = '')
   this.style.boxShadow = '0 0 0 4px yellow'
 }
 
+const aiChoice = function () {
+  return hands[Math.floor(Math.random() * 3)].dataset.option
+
+}
+
+// starting function 
+const startGame = function () {
+  if (!game.playerHand) return alert('take your pick')
+
+  game.aiHand = aiChoice()
+
+}
 hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+document.querySelector('.start').addEventListener('click', startGame);
