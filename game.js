cd0d5e0f86ec67eae7eb60e919279df66783e0fd
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-30 21:13:27 +0100

    add function checking the winner

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ const hands = [...document.querySelectorAll('.select img')];
 //player choice function 
 function handSelection() {
   game.playerHand = this.dataset.option;
-  console.log(game.playerHand)
+  // console.log(game.playerHand)
   hands.forEach(hand => hand.style.boxShadow = '')
   this.style.boxShadow = '0 0 0 4px yellow'
 }
@@ -25,11 +25,22 @@ const aiChoice = function () {
 
 }
 
-// starting function 
+const checkResult = (player, ai) => {
+  if (player === ai) {
+    return 'draw'
+  } else if ((player === 'paper' && ai === 'rock') || (player === 'rock' && ai === 'scissors') || (player === 'scissors' && ai === 'paper')) {
+    return 'win'
+  } else {
+    return 'loss'
+  }
+}
+
+// steering function 
 const startGame = function () {
   if (!game.playerHand) return alert('take your pick')
-
   game.aiHand = aiChoice()
+  const gameResult = checkResult(game.playerHand, game.aiHand)
+  console.log(gameResult)
 
 }
 hands.forEach(hand => hand.addEventListener('click', handSelection))
