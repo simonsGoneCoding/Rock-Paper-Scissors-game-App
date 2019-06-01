120012881fc4551dcb6bf81832e54af25423bc06
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-06-01 13:11:32 +0100

    add results publishing function

--- game.js
+++ game.js
@@ -20,11 +20,13 @@ function handSelection() {
   this.style.boxShadow = '0 0 0 4px yellow'
 }
 
+//computer choice function
 const aiChoice = function () {
   return hands[Math.floor(Math.random() * 3)].dataset.option
 
 }
 
+//checking results function 
 const checkResult = (player, ai) => {
   if (player === ai) {
     return 'draw'
@@ -35,13 +37,35 @@ const checkResult = (player, ai) => {
   }
 }
 
-// steering function 
+//publishing the results 
+const publishResult = (player, ai, result) => {
+  document.querySelector('[data-summary="your-choice"]').textContent = player;
+  document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+  document.querySelector('p.numbers span').textContent = ++gameSummary.numbers
+
+  if (result === 'win') {
+    document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+    document.querySelector('[data-summary="who-wins"]').style.color = 'green'
+    document.querySelector('[data-summary="who-wins"]').textContent = 'YOU';
+  } else if (result === 'draw') {
+    document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+    document.querySelector('[data-summary="who-wins"]').textContent = 'no one';
+    document.querySelector('[data-summary="who-wins"]').style.color = 'grey'
+  } else {
+    document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+    document.querySelector('[data-summary="who-wins"]').textContent = 'computer';
+    document.querySelector('[data-summary="who-wins"]').style.color = 'red'
+  }
+}
+
+//steering function 
 const startGame = function () {
   if (!game.playerHand) return alert('take your pick')
   game.aiHand = aiChoice()
   const gameResult = checkResult(game.playerHand, game.aiHand)
   console.log(gameResult)
-
+  publishResult(game.playerHand, game.aiHand, gameResult)
 }
 hands.forEach(hand => hand.addEventListener('click', handSelection))
 
